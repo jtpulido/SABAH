fe502c73eac68087110ff197de68ac3cbaeaba3a
jtpulido <jtpulido@unbosque.edu.co>
2023-04-13 09:13:24 -0500

    l

--- back-end/src/routes/inicio.routes.js
+++ back-end/src/routes/inicio.routes.js
@@ -1,6 +1,4 @@
 const { Router } = require('express')
-const passport = require('passport');
-const { check, validationResult } = require('express-validator');
 const { isLoggedIn, isNotLoggedIN } = require('../lib/auth');
 const { login } = require('../controllers/inicio.controller')
 const { register } = require('../controllers/usuario.controller')
@@ -11,6 +9,7 @@ router.post('/login', isNotLoggedIN, login);
 router.get('/logout', isLoggedIn, (req, res) => {
   req.logOut();
 });
+
 router.post('/register',isNotLoggedIN, register);
 
 module.exports = router;
