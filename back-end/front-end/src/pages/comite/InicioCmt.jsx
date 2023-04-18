dff6a119ab9ce19991aa20772d0ac6709d6be3e5
jtpulido <jtpulido@unbosque.edu.co>
2023-04-18 16:23:07 -0500

    ada

--- back-end/front-end/src/pages/comite/InicioCmt.jsx
+++ back-end/front-end/src/pages/comite/InicioCmt.jsx
@@ -1,52 +1,111 @@
-import * as React from 'react';
-import { Box, Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { clearUser } from '../../store/authSlice';
+import { Box, AppBar, Drawer, CssBaseline, List, ListItem, ListItemButton, ListItemText, Toolbar } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import logo from "../../assets/images/Sabah.png";
+import Footer from "../pie_de_pagina/Footer"
 import "./InicioCmt.css";
+import { Outlet } from 'react-router-dom';
 
 const drawerWidth = 240;
 
 function InicioCmt() {
-  const drawer = (
-    <div>
-      <Toolbar />
-      <Divider />
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
 
-    </div>
-  );
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  return (
-    <Box sx={{ display: 'flex' }}>
+  const cerrarSesion = () => {
+    dispatch(clearUser());
+    navigate('/')
+  };
+  const [activeButton, setActiveButton] = useState(null);
+  const buttonColors = {
+    proyecto: activeButton === "proyecto" ? "rgb(184, 207, 105)" : "rgb(255, 255, 255)",
+    lector: activeButton === "lector" ? "rgb(184, 207, 105)" : "rgb(255, 255, 255)",
+  };
 
+  const handleClick = (button) => {
+    setActiveButton(button);
+    if (button === "proyecto") {
+      navigate('/comite')
+    } else if (button === "lector") {
+      navigate('/comite/lectores')
+    }
+  };
+  return (<div><CssBaseline />
+    <Box sx={{ display: 'flex', height: 'calc(100vh - 50px)' }} >
       <Box sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}>
-        <Drawer variant="permanent" sx={{ display: 'block', '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }, }} open        >
-          {drawer}
-        </Drawer>
+        <ThemeProvider
+          theme={createTheme({
+            components: {
+              MuiListItemButton: {
+                defaultProps: {
+                  disableTouchRipple: true,
+                },
+              },
+            },
+            palette: {
+              primary: { main: 'rgb(184, 207, 105)' },
+            },
+          })}
+        >
+          <Drawer
+            variant="permanent"
+            sx={{
+              width: drawerWidth,
+              flexShrink: 0,
+              [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+            }}
+          >
+            <Toolbar> <img src={logo} alt="" style={{ width: '200px' }} /></Toolbar>
+            <Box sx={{ overflow: 'auto', ml: 2 }}>
+              <List >
+                <ListItem disablePadding>
+                  <ListItemButton onClick={() => handleClick("proyecto")} sx={{
+                    backgroundColor: buttonColors.proyecto,
+                    "&:hover": {
+                      backgroundColor: "rgb(184, 207, 105)",
+                    },
+                  }}>
+                    <ListItemText primary="PROYECTOS" sx={{ color: '#576A3D' }} />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListItemButton onClick={() => handleClick("lector")} sx={{
+                    backgroundColor: buttonColors.lector,
+                    "&:hover": {
+                      backgroundColor: "rgb(184, 207, 105)",
+                    },
+                  }}>
+                    <ListItemText primary="LECTORES" sx={{ color: '#576A3D' }} />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding >
+                  <ListItemButton onClick={() => cerrarSesion()} >
+                    <ListItemText primary="CERRAR SESIÓN" sx={{ color: '#576A3D' }} />
+                  </ListItemButton>
+                </ListItem>
+              </List>
 
+            </Box>
+
+            <Toolbar />
+          </Drawer>
+        </ThemeProvider>
       </Box>
-
-      <Box
-        sx={{ p: 1, width: { sm: `calc(100% - ${drawerWidth}px)` }, minHeight: '100vh', display: 'flex', flexDirection: 'column', }}      >
-        <Typography paragraph>
-          Inicio Comite
-        </Typography>
+      <Box component="main" sx={{ p: 1, width: { sm: `calc(100% - ${drawerWidth}px)` }, overflow: 'auto', minHeight: '100%' }}>
+        <Outlet />
 
       </Box>
 
     </Box>
+
+    <AppBar position="relative" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+      <Footer />
+    </AppBar>
+  </div >
   );
 }
 
