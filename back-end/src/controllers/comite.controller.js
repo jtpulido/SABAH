277c98f3d91a05d7d08db7f9786f93e420e28adb
dbenavidesg <57646538+dbenavidesg@users.noreply.github.com>
2023-04-23 21:42:25 -0500

    Squashed commit of the following:
    commit 22ce57c5e2d7eb6784b0dd2c3ddb487c824ddfb1
    Author: jtpulido <jtpulido@unbosque.edu.co>
    Date:   Sun Apr 23 14:32:49 2023 -0500
    
        Tablas de proyectos
    
    commit d8f9fc8ccf37fe181f694db88a5ca0cdc96e1cdf
    Author: jtpulido <jtpulido@unbosque.edu.co>
    Date:   Fri Apr 21 16:13:44 2023 -0500
    
        Tabla

--- back-end/src/controllers/comite.controller.js
+++ back-end/src/controllers/comite.controller.js
@@ -1,6 +1,34 @@
 const pool = require('../database')
 
-const obtenerProyectos = async (req, res) => {
+const obtenerProyectosDesarrollo = async (req, res) => {
+    try {
+        const result = await pool.query('SELECT p.id, p.codigo, p.nombre, p.anio, p.periodo, m.acronimo as modalidad, e.nombre as etapa, es.nombre as estado FROM proyecto p JOIN modalidad m ON p.id_modalidad = m.id JOIN etapa e ON p.id_etapa = e.id JOIN estado es ON p.id_estado = es.id WHERE es.id=1')
+        const proyectos = result.rows
+        if (result.rowCount > 0) {
+            return res.json({ success: true, proyectos });
+        } else {
+            return res.status(401).json({ success: true, message: 'No hay proyectos actualmente' })
+        }
+    } catch (error) {
+        res.status(502).json({ success: false, message: 'Lo siento, ha ocurrido un error. Por favor, intente de nuevo más tarde o póngase en contacto con el administrador del sistema para obtener ayuda.' });
+    }
+};
+
+const obtenerProyectosTerminados = async (req, res) => {
+    try {
+        const result = await pool.query('SELECT p.id, p.codigo, p.nombre, p.anio, p.periodo, m.acronimo as modalidad, e.nombre as etapa, es.nombre as estado FROM proyecto p JOIN modalidad m ON p.id_modalidad = m.id JOIN etapa e ON p.id_etapa = e.id JOIN estado es ON p.id_estado = es.id WHERE es.id <> 1')
+        const proyectos = result.rows
+        if (result.rowCount > 0) {
+            return res.json({ success: true, proyectos });
+        } else {
+            return res.status(401).json({ success: true, message: 'No hay proyectos actualmente' })
+        }
+    } catch (error) {
+        res.status(502).json({ success: false, message: 'Lo siento, ha ocurrido un error. Por favor, intente de nuevo más tarde o póngase en contacto con el administrador del sistema para obtener ayuda.' });
+    }
+};
+
+const obtenerTodosProyectos = async (req, res) => {
     try {
         const result = await pool.query('SELECT p.id, p.codigo, p.nombre, p.anio, p.periodo, m.nombre as modalidad, e.nombre as etapa, es.nombre as estado FROM proyecto p JOIN modalidad m ON p.id_modalidad = m.id JOIN etapa e ON p.id_etapa = e.id JOIN estado es ON p.id_estado = es.id')
         const proyectos = result.rows
@@ -15,4 +43,4 @@ const obtenerProyectos = async (req, res) => {
 };
 
 
-module.exports = { obtenerProyectos }
+module.exports = { obtenerTodosProyectos, obtenerProyectosTerminados, obtenerProyectosDesarrollo }
--- back-end/src/routes/comite.routes.js
+++ back-end/src/routes/comite.routes.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 const passport = require('passport');
-const { obtenerProyectos } = require('../controllers/comite.controller')
+const { obtenerTodosProyectos, obtenerProyectosTerminados, obtenerProyectosDesarrollo } = require('../controllers/comite.controller')
 const router = Router()
 
 
-router.get('/obtenerProyectos', passport.authenticate('jwt', { session: false }), obtenerProyectos);
-
+router.get('/proyectos/obtenerTodos', passport.authenticate('jwt', { session: false }), obtenerTodosProyectos);
+router.get('/proyectos/obtenerTerminados', passport.authenticate('jwt', { session: false }), obtenerProyectosTerminados);
+router.get('/proyectos/obtenerEnCurso', passport.authenticate('jwt', { session: false }), obtenerProyectosDesarrollo);
 
 module.exports = router;
