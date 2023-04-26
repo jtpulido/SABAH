20cf443fd984f0427a30a63e7619679d924f967c
jtpulido <jtpulido@unbosque.edu.co>
2023-04-25 23:17:41 -0500

    Ver proyectos

--- back-end/front-end/src/pages/comite/VerProyecto.jsx
+++ back-end/front-end/src/pages/comite/VerProyecto.jsx
@@ -15,8 +15,15 @@ export default function VerProyectos() {
   const colors = tokens(theme.palette.mode);
   const [error, setError] = useState(null);
   const handleClose = () => setError(null);
-
+  const [existe, setExiste] = useState([]);
   const [proyecto, setProyecto] = useState([]);
+  const [estudiantes, setEstudiantes] = useState([]);
+  const [director, setDirector] = useState([]);
+  const [lector, setLector] = useState([]);
+  const [existeLector, setExisteLector] = useState([]);
+  const [existeJurados, setExisteJurados] = useState([]);
+  const [listaJurado, setListaJurado] = useState([]);
+
   const infoProyecto = async () => {
     try {
       const response = await fetch("http://localhost:5000/comite/verProyecto", {
@@ -28,11 +35,20 @@ export default function VerProyectos() {
       const data = await response.json();
       if (!data.success) {
         setError(data.message);
+        setExiste(false)
       } else {
         setProyecto(data.proyecto);
+        setEstudiantes(data.estudiantes);
+        setDirector(data.director);
+        setExisteLector(data.lector.existe_lector)
+        setLector(data.lector.existe_lector ? data.lector.nombre : "");
+        setExisteJurados(data.jurados.existe_jurado)
+        setListaJurado(data.jurados.existe_jurado ? data.jurados.jurados : []);
+        setExiste(true)
       }
     }
     catch (error) {
+      setExiste(false)
       setError("Lo siento, ha ocurrido un error de autenticación. Por favor, intente de nuevo más tarde o póngase en contacto con el administrador del sistema para obtener ayuda.");
     }
   };
@@ -48,62 +64,159 @@ export default function VerProyectos() {
           </Alert>
         </Snackbar>
       )}
-      <CssBaseline />
-      <Typography
-        variant="h4"
-        color={colors.secundary[100]}
-      >
-        {proyecto.modalidad}
-      </Typography>
-      <Typography
-        variant="h5"
-      >
-        {proyecto.nombre}
-      </Typography>
-      <Typography
-        variant="h5"
-      >
-        {proyecto.codigo}
-      </Typography>
-      <Box >
-        <Typography variant="h6" color={colors.secundary[100]} sx={{ mt: "20px", mb: "20px" }}>
-          Información General
-        </Typography>
+      {existe ? (
 
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={6} md={4} lg={3}>
-            <Typography variant="h6" color={colors.primary[100]}>
-              Modalidad
-            </Typography>
-            <TextField value={proyecto.modalidad} fullWidth />
-          </Grid>
-          <Grid item xs={12} sm={6} md={4} lg={3}>
-            <Typography variant="h6" color={colors.primary[100]}>
-              Etapa
-            </Typography>
-            <TextField value={proyecto.etapa} fullWidth />
-          </Grid>
-          <Grid item xs={12} sm={6} md={4} lg={3}>
-            <Typography variant="h6" color={colors.primary[100]}>
-              Estado
-            </Typography>
-            <TextField value={proyecto.estado} fullWidth />
-          </Grid>
-          <Grid item xs={12} sm={6} md={4} lg={3}>
-            <Typography variant="h6" color={colors.primary[100]}>
-              Año
-            </Typography>
-            <TextField value={proyecto.anio} fullWidth />
-          </Grid>
-          <Grid item xs={12} sm={6} md={4} lg={3}>
-            <Typography variant="h6" color={colors.primary[100]}>
-              Período
-            </Typography>
-            <TextField value={proyecto.periodo} fullWidth />
-          </Grid>
+        <Box >
+          <CssBaseline />
 
-        </Grid>
-      </Box>
+          <Typography
+            variant="h4"
+            color={colors.secundary[100]}
+          >
+            {proyecto.modalidad || ''}
+          </Typography>
+          <Typography
+            variant="h5"
+          >
+            {proyecto.nombre || ''}
+          </Typography>
+          <Typography
+            variant="h5"
+          >
+            {proyecto.codigo || ''}
+          </Typography>
+          <Box >
+            <Typography variant="h6" color={colors.secundary[100]} sx={{ mt: "20px", mb: "20px" }}>
+              Información General
+            </Typography>
+
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6} md={4} lg={3}>
+                <Typography variant="h6" color={colors.primary[100]}>
+                  Modalidad
+                </Typography>
+                <TextField value={proyecto.modalidad || ''} fullWidth />
+              </Grid>
+              <Grid item xs={12} sm={6} md={4} lg={3}>
+                <Typography variant="h6" color={colors.primary[100]}>
+                  Etapa
+                </Typography>
+                <TextField value={proyecto.etapa || ''} fullWidth />
+              </Grid>
+              <Grid item xs={12} sm={6} md={4} lg={3}>
+                <Typography variant="h6" color={colors.primary[100]}>
+                  Estado
+                </Typography>
+                <TextField value={proyecto.estado || ''} fullWidth />
+              </Grid>
+              <Grid item xs={12} sm={6} md={4} lg={3}>
+                <Typography variant="h6" color={colors.primary[100]}>
+                  Año
+                </Typography>
+                <TextField value={proyecto.anio || ''} fullWidth />
+              </Grid>
+              <Grid item xs={12} sm={6} md={4} lg={3}>
+                <Typography variant="h6" color={colors.primary[100]}>
+                  Período
+                </Typography>
+                <TextField value={proyecto.periodo || ''} fullWidth />
+              </Grid>
+              <Grid item xs={12} sm={6} md={4} lg={3}>
+                <Typography variant="h6" color={colors.primary[100]}>
+                  Director
+                </Typography>
+                <TextField value={director.nombre || ''} fullWidth />
+              </Grid>
+
+              {proyecto.acronimo !== "AUX" && (
+                <>
+                  <Grid item xs={12} sm={6} md={4} lg={3}>
+                    <Typography variant="h6" color={colors.primary[100]}>Lector</Typography>
+                    {existeLector ? (
+                      <TextField value={lector || ''} xs={12} sm={6} md={4} lg={4} xl={3} fullWidth />
+                    ) : (
+                      <Typography variant="h6" color={colors.primary[100]}>No se han asignado lector</Typography>
+                    )}
+
+                  </Grid>
+                </>
+              )}
+            </Grid>
+          </Box>
+
+          <Box>
+            <Typography variant="h6" color={colors.secundary[100]} sx={{ mt: "20px", mb: "20px" }}>
+              Estudiante(s)
+            </Typography>
+
+            <Grid container>
+              {estudiantes.map((estudiante) => (
+                <Grid item key={estudiante.num_identificacion} xs={12}>
+                  <Grid container spacing={2}>
+                    <Grid item xs={12} sm={6} md={4} lg={4} xl={3}>
+                      <Typography variant="h6" color={colors.primary[100]}>
+                        Nombre
+                      </Typography>
+                      <TextField
+                        value={estudiante.nombre || ''}
+                        fullWidth
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} lg={4} xl={3}>
+                      <Typography variant="h6" color={colors.primary[100]}>
+                        Correo
+                      </Typography>
+                      <TextField
+                        value={estudiante.correo || ''}
+                        fullWidth
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} lg={4} xl={3}>
+                      <Typography variant="h6" color={colors.primary[100]}>
+                        Número de Identificación
+                      </Typography>
+                      <TextField
+                        value={estudiante.num_identificacion || ''}
+                        fullWidth
+                      />
+                    </Grid>
+                  </Grid>
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+          {proyecto.acronimo !== "AUX" && (
+            <> <Box>
+
+              <Typography variant="h6" color={colors.secundary[100]} sx={{ mt: "20px", mb: "20px" }}>
+                Jurado(s)
+              </Typography>
+              {existeJurados ? (
+
+
+                <Grid container spacing={2}>
+                  {listaJurado.map((jurado) => (
+                    <Grid item key={jurado.id} xs={12} sm={6} md={6} lg={6} xl={6}>
+                      <Typography variant="h6" color={colors.primary[100]}>
+                        Nombre
+                      </Typography>
+                      <TextField
+                        value={jurado.nombre || ''}
+                        fullWidth
+                      />
+                    </Grid>
+                  ))}
+                </Grid>
+
+              ) : (<Typography variant="h6" color={colors.primary[100]}>No se han asignado jurados</Typography>
+              )}
+            </Box>
+            </>
+          )}
+        </Box>
+      ) : (
+        <Typography variant="h6" color={colors.primary[100]}>{error}</Typography>
+      )}
     </div>
   );
 }
--- back-end/src/controllers/comite.controller.js
+++ back-end/src/controllers/comite.controller.js
@@ -46,25 +46,26 @@ const obtenerProyecto = async (req, res) => {
     const { id } = req.body;
     try {
         const error = "No se puedo encontrar toda la información relacionada al proyecto. Por favor, intente de nuevo más tarde o póngase en contacto con el administrador del sistema para obtener ayuda."
-        const result = await pool.query('SELECT p.id, p.codigo, p.nombre, p.anio, p.periodo, m.nombre as modalidad, e.nombre as etapa, es.nombre as estado FROM proyecto p JOIN modalidad m ON p.id_modalidad = m.id JOIN etapa e ON p.id_etapa = e.id JOIN estado es ON p.id_estado = es.id WHERE p.id = $1', [id])
+        const result = await pool.query('SELECT p.id, p.codigo, p.nombre, p.anio, p.periodo, m.nombre as modalidad, m.acronimo as acronimo, e.nombre as etapa, es.nombre as estado FROM proyecto p JOIN modalidad m ON p.id_modalidad = m.id JOIN etapa e ON p.id_etapa = e.id JOIN estado es ON p.id_estado = es.id WHERE p.id = $1', [id])
         const proyecto = result.rows
         if (result.rowCount === 1) {
-            const result2 = await pool.query('SELECT u.nombre AS nombre_usuario, r.nombre AS nombre_rol FROM usuario u INNER JOIN usuario_rol ur ON u.id = ur.id_usuario INNER JOIN rol r ON ur.id_rol = r.id WHERE ur.id_proyecto = $1', [id])
-            const usuario_rol = result2.rows
-            if (result2.rowCount > 0) {
-                const result3 = await pool.query('SELECT e.nombre AS nombre_estudiante FROM estudiante e INNER JOIN estudiante_proyecto ep ON e.id = ep.id_estudiante WHERE ep.id_proyecto = $1', [id])
-                const estudiantes = result3.rows
-                if (result3.rowCount > 0) {
-                    return res.json({ success: true, proyecto: proyecto[0], usuario_rol, estudiantes });
-                } else {
-                    return res.status(401).json({ success: true, message: error })
-                }
+
+            const result_director = await pool.query("SELECT u.nombre FROM usuario u INNER JOIN usuario_rol ur ON u.id = ur.id_usuario INNER JOIN rol r ON ur.id_rol = r.id WHERE UPPER(r.nombre)=UPPER('director') AND ur.id_proyecto = $1", [id])
+            const usuario_director = result_director.rows[0]
+            const result_lector = await pool.query("SELECT u.nombre FROM usuario u INNER JOIN usuario_rol ur ON u.id = ur.id_usuario INNER JOIN rol r ON ur.id_rol = r.id WHERE UPPER(r.nombre)=UPPER('lector') AND ur.id_proyecto = $1", [id])
+            const info_lector = result_lector.rowCount > 0 ? { "existe_lector": true, "nombre": result_lector.rows[0].nombre } : { "existe_lector": false };
+            const result_jurado = await pool.query("SELECT u.nombre, u.id FROM usuario u INNER JOIN usuario_rol ur ON u.id = ur.id_usuario INNER JOIN rol r ON ur.id_rol = r.id WHERE UPPER(r.nombre)=UPPER('jurado')AND ur.id_proyecto = $1", [id])
+            const info_jurado = result_jurado.rowCount > 0 ? { "existe_jurado": true, "jurados": result_jurado.rows } : { "existe_jurado": false };
+            const result_estudiantes = await pool.query('SELECT e.nombre, e.correo, e.num_identificacion FROM estudiante e INNER JOIN estudiante_proyecto ep ON e.id = ep.id_estudiante WHERE ep.id_proyecto = $1', [id])
+
+            if (result_estudiantes.rowCount > 0 && result_director.rowCount > 0) {
+                return res.json({ success: true, proyecto: proyecto[0],director: usuario_director, jurados: info_jurado, lector: info_lector, estudiantes: result_estudiantes.rows });
             } else {
                 return res.status(401).json({ success: true, message: error })
             }
 
         } else {
-            return res.status(401).json({ success: true, message: 'No se encontro el proyecto' })
+            return res.status(401).json({ success: false, message: 'Ha ocurrido un error inesperado. Por favor, intente de nuevo más tarde o póngase en contacto con el administrador del sistema para obtener ayuda.' })
         }
     } catch (error) {
         res.status(502).json({ success: false, message: 'Lo siento, ha ocurrido un error. Por favor, intente de nuevo más tarde o póngase en contacto con el administrador del sistema para obtener ayuda.' });
