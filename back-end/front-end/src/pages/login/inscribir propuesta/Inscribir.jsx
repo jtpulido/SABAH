169c786588a38e4d58e45662543f9eeaf92e42d8
dbenavidesg <57646538+dbenavidesg@users.noreply.github.com>
2023-09-14 08:01:25 -0500

    Cambio inscribir propuesta

--- back-end/front-end/src/pages/login/inscribir propuesta/Inscribir.jsx
+++ back-end/front-end/src/pages/login/inscribir propuesta/Inscribir.jsx
@@ -150,6 +150,8 @@ export const Inscribir = () => {
     const generateCode = (consecutivo) => {
         const now = new Date();
         const formattedDate = getFormattedDate();
+        const month = (now.getMonth() + 1).toString().padStart(2, "0");
+        const periodo = getPeriodo(month);
         return `TEM_${formattedDate}-${periodo}-${consecutivo}`;
     };
 
